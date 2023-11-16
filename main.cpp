0a261b4ad317aff4213c8e8642fd642e908285e4
ArturoDuCas <a00833516@tec.mx>
2023-11-16 08:46:58 -0600

    initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/*
+    Actividad Integradora 2 - TC2038
+    Integrantes:
+        Arturo Duran Castillo - A00833516
+        Andrés Marcelo De Luna Pámanes - A00832239
+    Fecha: 26 de noviembre 2023
+ */
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    cout << "Hello Marcelo!" << endl;
+
+    return 0;
+}
