6c314704ad26741ebdb8d6bb3cf8dbdd9175e173
ArturoDuCas <a00833516@tec.mx>
2023-11-25 20:31:35 -0600

    feat: create adjMat

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
  */
 
 #include <iostream>
+#include <climits>
 #include <vector>
 #include <algorithm>
 #include <map>
@@ -14,14 +15,23 @@
 using namespace std;
 
 struct Colonia {
-    string nombre;
-    int x, y, central;
+    string name;
+    int x, y, isCentral;
 };
 
 struct Conexion {
     string colonia1, colonia2;
     int costo;
 };
+
+void printVecOfVecOfInt(vector<vector<int>> &vec) {
+    for(int i = 0; i < vec.size(); i++) {
+        for(int j = 0; j < vec.size(); j++) {
+            cout << vec[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
 
 struct ConjuntoDisjunto {
     map<string, string> indiceColonias;
@@ -64,22 +74,38 @@ vector<Conexion> encontrarMST(vector<Conexion> &conexiones, int n, ConjuntoDisju
 }
 
 int main() {
-    int n, m, central, k;
-    cin >> n >> m >> central >> k;
+    // n = number of neighborhoods
+    // m = number of connections
+    // k = connections with new cable
+    // q = number of new neighborhoods
+    int n, m, k, q;
+    cin >> n >> m >> k >> q;
 
     vector<Colonia> colonias(n);
     vector<Conexion> conexiones(m);
+    // TODO: verify if they are always int values
+    vector<vector<int>> matAdj(n, vector<int>(n, INT_MAX)); // for tsp
+    unordered_map<string, int> neighborhoodsIndex;
+
 
     ConjuntoDisjunto conjDisjunto;
 
+    // Read neighborhoods
     for (int i = 0; i < n; ++i) {
-        cin >> colonias[i].nombre >> colonias[i].x >> colonias[i].y >> colonias[i].central;
+        cin >> colonias[i].name >> colonias[i].x >> colonias[i].y >> colonias[i].isCentral;
+        neighborhoodsIndex[colonias[i].name] = i;
         // Assign index to colony names
-        conjDisjunto.indiceColonias[colonias[i].nombre] = colonias[i].nombre;
+        conjDisjunto.indiceColonias[colonias[i].name] = colonias[i].name;
     }
 
+    // Read connections
     for (int i = 0; i < m; ++i) {
         cin >> conexiones[i].colonia1 >> conexiones[i].colonia2 >> conexiones[i].costo;
+
+        // Add edge to adjacency matrix
+        int u = neighborhoodsIndex[conexiones[i].colonia1];
+        int v = neighborhoodsIndex[conexiones[i].colonia2];
+        matAdj[u][v] = matAdj[v][u] = conexiones[i].costo;
     }
 
     vector<Conexion> mst = encontrarMST(conexiones, n, conjDisjunto);
